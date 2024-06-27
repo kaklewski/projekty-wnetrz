2ec25793e08d8d929eb0a0487eb24690514d5f92
Oskar <oskarkaklewski@gmail.com>
2024-06-27 23:08:31 +0200

    Replaced HTML in JS with an HTML template

--- src/JS/populate-portfolio-items.js
+++ src/JS/populate-portfolio-items.js
@@ -7,41 +7,18 @@ import { interiors, bathrooms } from './projects-dictionary';
 const interiorsContainer = document.querySelector('.interiors > .item-wrapper');
 const bathroomsContainer = document.querySelector('.bathrooms > .item-wrapper');
 
-// for each portfolio item create an item card and add it to the container
+const portfolioItemTemplate = document.querySelector(
+	'.portfolio-item-template'
+);
+
+// for each portfolio item create an item card based on a template and add it to the container
 function populatePortfolioItems(projects, itemsContainer) {
-	for (const project in projects) {
-		const portfolioItem = document.createElement('div');
-		portfolioItem.classList.add('portfolio-item');
+	for (const projectName in projects) {
+		const portfolioItem = portfolioItemTemplate.content.cloneNode(true);
 
-		let portfolioItemContent = `
-					<a href="/projekt/?id=${project}"></a>
-					<div class="portfolio-item__shadow">
-						<span class="portfolio-item__shadow__text">
-							<span>Zobacz cały projekt</span>
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								class="icon icon-tabler icon-tabler-chevrons-right"
-								width="24"
-								height="24"
-								viewBox="0 0 24 24"
-								stroke-width="1.5"
-								stroke="currentColor"
-								fill="none"
-								stroke-linecap="round"
-								stroke-linejoin="round">
-								<path
-									stroke="none"
-									d="M0 0h24v24H0z"
-									fill="none" />
-								<path d="M7 7l5 5l-5 5" />
-								<path d="M13 7l5 5l-5 5" />
-							</svg>
-						</span>
-					</div>
-					<img loading="lazy" src="images/${project}/1.webp" alt="" />
-				`;
+		portfolioItem.querySelector('a').href = `/projekt/?id=${projectName}`;
+		portfolioItem.querySelector('img').src = `images/${projectName}/1.webp`;
 
-		portfolioItem.innerHTML = portfolioItemContent;
 		itemsContainer.appendChild(portfolioItem);
 	}
 }
